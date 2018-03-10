f4c4e1d025a60cf141eb9df24205b7742eb504cf
Michiel Van Eester <michiel.vaneester@immalle.be>
2018-03-10 12:37:10 +0100

    Model added

---
+++ WebApplication2/Models/Films.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication2.Models
+{
+    class Films
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
